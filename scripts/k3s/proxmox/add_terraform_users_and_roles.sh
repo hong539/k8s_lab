92c43af1adfe5a9a3041ea784c4ee32a8c76e0b7
Desert <52050772+hong539@users.noreply.github.com>
2025-04-02 17:06:25 +0800

    Update add_terraform_users_and_roles.sh
    Fix userid
    From [docs](https://pve.proxmox.com/pve-docs/pveum.1.html):
    ```
    pveum user token add <userid> <tokenid> [OPTIONS] [FORMAT_OPTIONS]
    ```

--- scripts/k3s/proxmox/add_terraform_users_and_roles.sh
+++ scripts/k3s/proxmox/add_terraform_users_and_roles.sh
@@ -6,7 +6,7 @@ pveum user add terraform-prov@pve --password '<password>'
 pveum aclmod / -user terraform-prov@pve -role TerraformProv
 
 #api token
-pveum user token add 'terraform-prov' 'pve'
+pveum user token add 'terraform-prov@pve' 'pve'
 
 #Proxmox > 8:
 # pveum role modify TerraformProv -privs "Datastore.AllocateSpace Datastore.AllocateTemplate Datastore.Audit Pool.Allocate Sys.Audit Sys.Console Sys.Modify VM.Allocate VM.Audit VM.Clone VM.Config.CDROM VM.Config.Cloudinit VM.Config.CPU VM.Config.Disk VM.Config.HWType VM.Config.Memory VM.Config.Network VM.Config.Options VM.Migrate VM.Monitor VM.PowerMgmt SDN.Use"
